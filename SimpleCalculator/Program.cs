49aafb10dc876ae0290532f5347e107f66c1e0bb
lukepadiachy <148848073+lukepadiachy@users.noreply.github.com>
2023-11-28 13:44:44 +0200

    Changed Variable and String Message
    Experimenting with GitHub

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -5,12 +5,12 @@
         static void Main(string[] args)
         {
 
-            Console.Write(" Enter a number ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write(" Enter a  second number ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write(" Enter a Number ");
+            double num10 = Convert.ToDouble(Console.ReadLine());
+            Console.Write(" Enter a  Second Number ");
+            double num20 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine(num1 + num2);
+            Console.WriteLine(num10 + num20);
 
 
             Console.ReadLine();
