feea497c8b6a63891106df88a2ebefc5e3731366
lukepadiachy <148848073+lukepadiachy@users.noreply.github.com>
2023-11-28 11:59:31 +0200

    First Commit

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write(" Enter a number ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write(" Enter a  second number ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
